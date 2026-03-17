cee5fa29c73e59a426c4250498b7ce0073bd5a58
Itx_Taha <mrtaha09876@gmail.com>
2026-03-17 13:54:59 +0500

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,16 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "PAKWEBMASTER",
+  description: "We create and launch websites for growing businesses."
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
