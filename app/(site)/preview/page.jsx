4175dbc23d5134b50eb409037ccdefee350b9077
Itx_Taha <mrtaha09876@gmail.com>
2026-03-18 15:40:09 +0500

    8

--- app/(site)/preview/page.jsx
+++ app/(site)/preview/page.jsx
@@ -1,11 +1,13 @@
 import HeroSection from "../../../sections/HeroSection";
 import ShowcaseSection from "../../../sections/ShowcaseSection";
+import PressSection from "../../../sections/PressSection";
 
 export default function PreviewPage() {
   return (
     <>
       <HeroSection />
       <ShowcaseSection />
+      <PressSection />
     </>
   );
 }
